12d5e9fc109937caebd149648d8d4281d90b6c7b
alkeops <m_adan2012@outlook.com>
2021-02-24 17:55:50 -0600

    Compiled ES5

--- Reto/dist/index.js
+++ Reto/dist/index.js
@@ -1,30 +1,37 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.serverHttp = void 0;
 var express_1 = __importDefault(require("express"));
 var express_handlebars_1 = __importDefault(require("express-handlebars"));
+var http_1 = __importDefault(require("http"));
 var constants_1 = require("./constants");
 var app = express_1.default();
-app.engine("hbs", express_handlebars_1.default({
+exports.serverHttp = http_1.default.createServer(app);
+app.engine(
+  "hbs",
+  express_handlebars_1.default({
     extname: ".hbs",
     defaultLayout: "index.hbs",
     layoutsDir: __dirname + "/views/layouts",
     partialsDir: __dirname + "/views/partials",
-}));
+  })
+);
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
-app.use(express_1.default.static("public"));
+app.use(express_1.default.static("../public"));
 app.use("/api", require("./routes/products"));
 app.set("view engine", "hbs");
-app.set("views", "./src/views");
-app.get("/api", function (req, res) {
-    res.render("main", { suggestedChamps: [] });
-});
+app.set("views", "./views");
 var PORT = 8080;
 var HOST = "127.0.0.1";
-var server = app.listen(PORT, HOST, function () {
-    return console.log(constants_1.SERVER_RUNNING + " " + HOST + ":" + PORT);
+var server = exports.serverHttp.listen(PORT, HOST, function () {
+  return console.log(constants_1.SERVER_RUNNING + " " + HOST + ":" + PORT);
 });
-server.on("error", function (error) { return console.log(constants_1.SERVER_ERROR + " " + error); });
+server.on("error", function (error) {
+  return console.log(constants_1.SERVER_ERROR + " " + error);
+});
--- Reto/dist/routes/products.js
+++ Reto/dist/routes/products.js
@@ -1,20 +1,53 @@
 "use strict";
-var __spreadArrays = (this && this.__spreadArrays) || function () {
-    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
-    for (var r = Array(s), k = 0, i = 0; i < il; i++)
-        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
-            r[k] = a[j];
-    return r;
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var fs = require("fs");
 var States_1 = __importDefault(require("../States"));
+var index_1 = require("../index");
 var constants_1 = require("../constants");
+if (!fs.existsSync("./mensajes.txt"))
+    fs.writeFileSync("./mensajes.txt", JSON.stringify([]));
+var io = require("socket.io")(index_1.serverHttp);
 var router = express_1.default.Router();
 var _a = new States_1.default(), state = _a.state, setState = _a.setState, deleteState = _a.deleteState, stateExists = _a.stateExists, updateState = _a.updateState;
+var Mensajes = new States_1.default();
+/*SOCKETS*/
+io.on("connection", function (socket) {
+    socket.emit("productos", state.length > 0 ? state : constants_1.NO_PRODUCTS);
+    socket.on("producto", function (producto) {
+        var title = producto.title, price = producto.price, thumbnail = producto.thumbnail;
+        var newProduct = {
+            title: title,
+            price: price,
+            thumbnail: thumbnail,
+            id: (state.length + 101).toString(),
+        };
+        setState(newProduct);
+        io.emit("productoCargado", state);
+    });
+});
+io.on("connection", function (socket) {
+    var read = fs.readFileSync("./mensajes.txt");
+    var data = JSON.parse(read);
+    var mensajesGuardados = data || [];
+    socket.emit("mensajes", mensajesGuardados.length > 0 ? mensajesGuardados : "Sin mensajes");
+    socket.on("mensaje", function (mensaje) {
+        var email = mensaje.email, sms = mensaje.sms;
+        var newMensaje = {
+            email: email,
+            sms: sms,
+            date: "" + new Date().toLocaleString("es-MX"),
+            id: (data.length + 1).toString(),
+        };
+        data.push(newMensaje);
+        fs.writeFileSync("./mensajes.txt", JSON.stringify(data));
+        io.emit("mensajeEnviado", newMensaje);
+    });
+});
+/*Rutas*/
 router.get("/productos", function (req, res) {
     res.json(state.length > 0 ? state : constants_1.NO_PRODUCTS);
 });
@@ -31,7 +64,7 @@ router.post("/productos", function (req, res) {
     res.json(newProduct);
 });
 router.get("/productos/vista", function (req, res) {
-    res.render("main", { suggestedChamps: __spreadArrays(state) });
+    res.render("main");
 });
 router.get("/productos/:id", function (req, res) {
     var id = req.params.id;
